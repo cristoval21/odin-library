61da69564b7ef881f8241e0685f3f57e431aa7d4
Cristoval Wenoch <wenochcristoval@gmail.com>
2024-03-22 06:42:08 +0700

    wip: change to cards

--- script.js
+++ script.js
@@ -11,13 +11,10 @@ const myLibrary = [
   new Book("Sheldon, Sidney", "Bloodline", 464, true)
 ];
 
-function addBookToLibrary(author, title, numOfPages, haveRead) {
-  const book = new Book(author, title, numOfPages, haveRead);
-  myLibrary.push(book);
-}
-
 const bookList = document.querySelector(".book-list");
-fillTable();
+for (let book of myLibrary) {
+  fillTable(book);
+}
 
 const buttonAddBook = document.querySelector(".button-add-book");
 const dialogAddBook = document.querySelector(".dialog-add-book");
@@ -33,36 +30,47 @@ const inputFormPages = document.querySelector("#pages");
 const inputFormRead = document.querySelector("#read");
 buttonFormAddBook.addEventListener("click", () => {
   event.preventDefault();
-  addBookToLibrary(
+
+  const book = new Book(
     inputFormAuthor.value,
     inputFormTitle.value,
     inputFormPages.value,
     inputFormRead.checked
-  );
+  )
+  
+  myLibrary.push(book);
+  fillTable(book);
   dialogAddBook.close();
-  fillTable();
 });
 buttonFormCancel.addEventListener("click", () => {
   dialogAddBook.close();
 });
 
-function fillTable() {
-  for (let book of myLibrary) {
-    let rowItem = document.createElement("tr");
-    let author = document.createElement("td");
-    let title = document.createElement("td");
-    let numOfPages = document.createElement("td");
-    let haveRead = document.createElement("td");
+function fillTable(book) {
+  let rowItem = document.createElement("tr");
+  let author = document.createElement("td");
+  let title = document.createElement("td");
+  let numOfPages = document.createElement("td");
+  let haveRead = document.createElement("td");
+  let remove = document.createElement("td");
 
-    author.textContent = book.author;
-    title.textContent = book.title;
-    numOfPages.textContent = book.numOfPages;
-    haveRead.textContent = book.haveRead;
+  author.textContent = book.author;
+  title.textContent = book.title;
+  numOfPages.textContent = book.numOfPages;
+  haveRead.textContent = book.haveRead;
+  remove.textContent = "delete";
 
-    rowItem.appendChild(author);
-    rowItem.appendChild(title);
-    rowItem.appendChild(numOfPages);
-    rowItem.appendChild(haveRead);
-    bookList.appendChild(rowItem);
-  }
+  rowItem.classList.add("book-list-row");
+  author.classList.add("book-list-cell");
+  title.classList.add("book-list-cell");
+  numOfPages.classList.add("book-list-cell");
+  haveRead.classList.add("book-list-cell");
+  remove.classList.add("material-symbols-outlined");
+
+  rowItem.appendChild(author);
+  rowItem.appendChild(title);
+  rowItem.appendChild(numOfPages);
+  rowItem.appendChild(haveRead);
+  rowItem.appendChild(remove);
+  bookList.appendChild(rowItem);
 }
