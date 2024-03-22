e3efff3ecc2679c48c32ee729dfce0caae9bb762
Cristoval Wenoch <wenochcristoval@gmail.com>
2024-03-23 06:41:00 +0700

    wip: change to cards

--- script.js
+++ script.js
@@ -12,9 +12,7 @@ const myLibrary = [
 ];
 
 const bookList = document.querySelector(".book-list");
-for (let book of myLibrary) {
-  fillTable(book);
-}
+refreshCards();
 
 const buttonAddBook = document.querySelector(".button-add-book");
 const dialogAddBook = document.querySelector(".dialog-add-book");
@@ -39,38 +37,65 @@ buttonFormAddBook.addEventListener("click", () => {
   )
   
   myLibrary.push(book);
-  fillTable(book);
+  refreshCards();
   dialogAddBook.close();
 });
 buttonFormCancel.addEventListener("click", () => {
   dialogAddBook.close();
 });
 
-function fillTable(book) {
-  let rowItem = document.createElement("tr");
-  let author = document.createElement("td");
-  let title = document.createElement("td");
-  let numOfPages = document.createElement("td");
-  let haveRead = document.createElement("td");
-  let remove = document.createElement("td");
+function refreshCards() {
+  const libraryWrapper = document.querySelector(".library-wrapper");
+  libraryWrapper.textContent = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
+    const card = document.createElement("div");
+    const cardHeader = document.createElement("div");
+    const cardAuthor = document.createElement("p");
+    const cardPages = document.createElement("p");
+    const cardTitle = document.createElement("p");
+    const cardButtons = document.createElement("div");
+    const cardReadButton = document.createElement("button");
+    const cardDeleteButton = document.createElement("button");
+  
+    card.setAttribute("data-index", i);
+    card.classList.add("book-card");
+    cardHeader.classList.add("book-card__header");
+    cardAuthor.className = "book-card__author book-card__pill";
+    cardPages.className = "book-card__pages book-card__pill";
+    cardTitle.classList.add("book-card__title");
+    cardButtons.classList.add("book-card__buttons");
+    cardReadButton.className = "book-card__button button material-symbols-outlined";
+    cardDeleteButton.className = "book-card__button button button--secondary material-symbols-outlined";
 
-  author.textContent = book.author;
-  title.textContent = book.title;
-  numOfPages.textContent = book.numOfPages;
-  haveRead.textContent = book.haveRead;
-  remove.textContent = "delete";
+    if (book.haveRead) {
+      cardReadButton.classList.add("button--primary");
+    } else {
+      cardReadButton.classList.add("button--secondary");
+    }
+  
+    cardAuthor.textContent = book.author;
+    cardPages.textContent = book.numOfPages;
+    cardTitle.textContent = book.title;
+    cardReadButton.textContent = "check_circle";
+    cardDeleteButton.textContent = "delete";
 
-  rowItem.classList.add("book-list-row");
-  author.classList.add("book-list-cell");
-  title.classList.add("book-list-cell");
-  numOfPages.classList.add("book-list-cell");
-  haveRead.classList.add("book-list-cell");
-  remove.classList.add("material-symbols-outlined");
+    cardDeleteButton.addEventListener("click", removeBook(card.getAttribute("data-index")));
+  
+    cardHeader.appendChild(cardAuthor);
+    cardHeader.appendChild(cardPages);
+    
+    cardButtons.appendChild(cardReadButton);
+    cardButtons.appendChild(cardDeleteButton);
+  
+    card.appendChild(cardHeader);
+    card.appendChild(cardTitle);
+    card.appendChild(cardButtons);
+  
+    libraryWrapper.appendChild(card);
+  }
+}
 
-  rowItem.appendChild(author);
-  rowItem.appendChild(title);
-  rowItem.appendChild(numOfPages);
-  rowItem.appendChild(haveRead);
-  rowItem.appendChild(remove);
-  bookList.appendChild(rowItem);
+function removeBook(index) {
+  // myLibrary.splice(index, 1);
 }
